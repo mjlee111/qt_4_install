6b87bb2169cf15339d3810b45f1501b282d7a081
mjlee111 <66550892+mjlee111@users.noreply.github.com>
2023-02-09 13:37:00 +0900

    Add files via upload

---
+++ qt4_install_for_arm64.sh
@@ -0,0 +1,24 @@
+echo "[RO:BIT] Installing QT4 for ARM64"
+echo "[RO:BIT] made by 17th myeungjin lee"
+echo ""
+echo ""
+echo ""
+
+echo "[RO:BIT] Installing..."
+sudo apt-get install -y qt4-dev-tools 
+sudo apt-get install -y qt4-qmake 
+sudo apt-get install -y libqt4-dev 
+sudo apt-get install -y libqtcore4
+sudo apt-get install -y libqtgui4
+sudo apt-get install -y qt4-designer
+sudo apt-get install -y qt4-dev-tools
+sudo apt-get install -y qtcreator
+sudo apt-get install -y g++
+echo ""
+echo ""
+echo ""
+
+echo "[RO:BIT] Done"
+
+
+
